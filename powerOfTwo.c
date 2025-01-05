cfb094263ede5a1cb98edb4919bdcb6ebfac25da
Krishh Singh <154503027+Luffers14@users.noreply.github.com>
2025-01-05 23:00:11 +0530

    Add files via upload

---
+++ powerOfTwo.c
@@ -0,0 +1,26 @@
+// Program to find whether a number is power of two 
+
+#include <stdio.h>
+
+int main()
+{
+  int n,ct = 0;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+
+  while (n>0)
+  {
+    ct += n & 1; 
+    n >>= 1;        // Right shift to process the next bit
+  }
+
+  if (n<=0 || ct!=1)    // if the number is negative or zero or has number of count bits other than zero it is not a power of two.
+  {
+    printf("The number is not a power of two.");
+  }
+  else{
+    printf("The number is a power of two");
+  }
+  
+  return 0;
+}
