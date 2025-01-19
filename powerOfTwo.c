52e045b1c450278724b5c0aad23ae2d92aff3d66
Krishh Singh <154503027+Luffers14@users.noreply.github.com>
2025-01-19 13:05:32 +0530

    Update powerOfTwo.c

--- powerOfTwo.c
+++ powerOfTwo.c
@@ -7,11 +7,12 @@ int main()
   int n,ct = 0;
   printf("Enter an integer: ");
   scanf("%d", &n);
-
-  while (n>0)
+  int temp = n;
+  
+  while (temp>0)
   {
-    ct += n & 1; 
-    n >>= 1;        // Right shift to process the next bit
+    ct += temp & 1; 
+    temp >>= 1;        // Right shift to process the next bit
   }
 
   if (n<=0 || ct!=1)    // if the number is negative or zero or has number of count bits other than one it is not a power of two.
